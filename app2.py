a0e849d23a15200a819a3941af55ebafada2f69b
Aravind Singirikonda <aravind0136@gmail.com>
2020-06-14 11:44:47 -0400

    todo app

--- app2.py
+++ app2.py
@@ -1,15 +1,36 @@
 from flask import Flask
 from flask_cors import CORS
+import sqlite3
+import json
+
+
 app = Flask(__name__)
 CORS(app)
+
 
 
 @app.route('/')
 def hello():
     return "Hello From Server!"
 
-@app.route('/mypath/')
-def hello2():
-    return "<h1>Hello World 2</h1>"
+@app.route('/addtodo/<todo>')
+def addtodo(todo):
+    conn = sqlite3.connect('mytodos.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO todos VALUES ('"+todo+"')")
+    conn.commit()
+    conn.close()
+    return "SUCCESS"
 
-app.run(port="1307")
+@app.route('/gettodos/')
+def gettodos():
+    conn = sqlite3.connect('mytodos.db')
+    c = conn.cursor()
+    c.execute("SELECT rowid, todo FROM todos")
+    todos = c.fetchall()
+    todo_array = []
+    for t in todos:
+        todo_array.append(t[1])
+    return json.dumps(todo_array)
+
+app.run(port="1309")
